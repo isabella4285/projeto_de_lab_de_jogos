41185dd85a2d7ab7000d6c45c50ccdfd0b206b29
isabella <guimaraesisabella8012@gmail.com>
2026-05-17 14:17:01 -0300

    Update and rename jogar2.py to main.py

--- pre_projeto/spaceinvaders/main.py
+++ pre_projeto/spaceinvaders/main.py
@@ -9,36 +9,43 @@ import os
 def jogar(jx, jy, dificuldade):
     run = True
     janela = Window(jx, jy)
-    janela.set_title("Space Invaders")
-    janela.set_background_color((0, 0, 0))
+    janela.set_title("Teste")
+    #janela.set_background_color((0, 0, 255))
     
     niveis = {'facil': 0.5, "medio": 1, "dificil":1.5}
     intervalo_tiro = niveis[dificuldade]
     
     tiros = []
     inimigas = []
+    nuvens_passadas =[]
     vel_tiro = 500 
     vel_inimiga = 150 # Aumentado para usar com dt
     
     shoot_sound = Sound("shoot.mp3")
     explosion_sound = Sound("explosion.mp3")
     time, time2 = 0 , 0
+    time3 = 0 #a velocidade dos objetos de fundo
     intervalos_inimigas = 2 # Diminuído para 2 segundos para testar mais rápido
     tempo_passado = 0
 
     #score basico
     acertos = 0
-    abatido = False
+    
     
     # Imagem nave
     nave = Sprite("spaceinvaders/images/nave.png")
     nave.set_position(jx/2 - nave.width/2, jy - nave.height)
 
+    #nuvens
+    nuvens = ["nuvens/nuvens(1).png", "nuvens/nuvens(2).png", "nuvens/nuvens(3).png", "nuvens/nuvens(4).png", "nuvens/nuvens(6).png"]
+
     while run:
-        janela.set_background_color((0, 0, 0))
+
+        janela.set_background_color((4, 0, 255))
         dt = janela.delta_time()
         time += dt
         time2 += dt
+        time3 += dt
         tempo_passado += dt
         
         keyboard = janela.get_keyboard()
@@ -74,6 +81,20 @@ def jogar(jx, jy, dificuldade):
             tiro.y -= vel_tiro * dt
             tiro.draw()
             
+        # Criando nuvens 
+        if time3 >= 1:
+            nuvem = Sprite(random.choice(nuvens))
+            # Sorteio dinâmico para não nascerem no mesmo lugar
+            nuvem.x = random.randint(0, int(jx - nuvem.width))
+            nuvem.y = -nuvem.height
+            nuvens_passadas.append(nuvem)
+            time3 = 0
+
+        for nuvem in nuvens_passadas:
+            nuvem.y += 150 * dt#muda isso aqui depois, para parecer que apenas os avioes estão acelerando
+            nuvem.draw()
+
+
         # Criando inimigos (Corrigido o >=)
         if time2 >= intervalos_inimigas:
             inimiga = Sprite("enemy.png")
@@ -85,29 +106,22 @@ def jogar(jx, jy, dificuldade):
 
         # Movimentando e desenhando inimigos (Adicionado * dt)
         for n_inimiga in inimigas:
-            n_inimiga.y += vel_inimiga * dt
+            n_inimiga.y += vel_inimiga * dt 
             n_inimiga.draw()
             
+
         # Remoção segura de objetos fora da tela (List Comprehension) --nao
 
         tiros = [tiro for tiro in tiros if tiro.y >= -tiro.height]
+        nuvens_passadas = [nuvem for nuvem in nuvens_passadas if nuvem.y<=jy]
         
         for n_inimiga in inimigas:
             if n_inimiga.y >= jy:
                 os.system('clear')
                 print("A nave conseguiu passar.\n Game Over")
                 print("Naves abatidas: ", acertos)
-                run = False
-
-
-
-        '''
-        for n_inimiga in inimigas:
-                    n_inimiga.y += vel_inimiga
-                    n_inimiga.draw()
-                for n_inimiga in inimigas:
-                    if n_inimiga.y==jy:      
-                        inimigas.remove(n_inimiga)'''
+                from gameover import game_over
+                game_over(800, 600)
 
 
         #detectando colisoes
@@ -124,6 +138,8 @@ def jogar(jx, jy, dificuldade):
                 os.system('clear')
                 print("NAVE ABATIDA!")
                 print("Aviões derrubados: ", acertos)
+                from gameover import game_over
+                game_over(800, 600)
         
         #aumentar a velocidade do inimigo conforme o tempo passa
         if tempo_passado > 2:
