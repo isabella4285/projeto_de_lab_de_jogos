64342fba28c553e35fe8992350d77316a252a7f0
isabella <guimaraesisabella8012@gmail.com>
2026-05-17 13:05:47 -0300

    Enhance gameplay mechanics and collision handling
    Refactor game logic and improve collision detection.

--- pre_projeto/spaceinvaders/jogar2.py
+++ pre_projeto/spaceinvaders/jogar2.py
@@ -3,98 +3,110 @@ from PPlay.sprite import *
 from PPlay.mouse import *
 import random
 from PPlay.sound import *
+from PPlay.collision import *
+import os
 
 def jogar(jx, jy, dificuldade):
     run = True
     janela = Window(jx, jy)
     janela.set_title("Space Invaders")
     janela.set_background_color((0, 0, 0))
+    
     niveis = {'facil': 0.5, "medio": 1, "dificil":1.5}
     intervalo_tiro = niveis[dificuldade]
+    
     tiros = []
     inimigas = []
-    vel_tiro = 500 #usando dt
-    shoot_sound = Sound("shoot.mp3")
-    time = 0 
-    intervalos_inimigas = 0
-    vel_inimiga = 1
-    #adicionar deltatime
+    vel_tiro = 500 
+    vel_inimiga = 150 # Aumentado para usar com dt
     
-    #imagem nave
+    shoot_sound = Sound("shoot.mp3")
+    explosion_sound = Sound("explosion.mp3")
+    time, time2 = 0 , 0
+    intervalos_inimigas = 2 # Diminuído para 2 segundos para testar mais rápido
+
+    #score basico
+    acertos = 0
+    abatido = False
+    
+    # Imagem nave
     nave = Sprite("spaceinvaders/images/nave.png")
-    nave.set_position(jx/2-nave.width/2, jy-nave.height)
+    nave.set_position(jx/2 - nave.width/2, jy - nave.height)
 
-    pos = random.randint(int(nave.width), jx)
-    #inimiga = Sprite("enemy.png")
-    #inimiga.set_position(pos, 0)
-
-    while True:
+    while run:
         janela.set_background_color((0, 0, 0))
         dt = janela.delta_time()
         time += dt
-        intervalos_inimigas += dt
+        time2 += dt
+        
         keyboard = janela.get_keyboard()
         if keyboard.key_pressed("ESC"):
             from menu import menu
             menu()
             run = False
-    
-        #movimentacao horizontal da nave
+        if acertos == 5:
+            print("Aviões derrubados: ", acertos)
+            run = False
+
+        # Movimentação horizontal da nave
         if keyboard.key_pressed("LEFT"):
-            if nave.x<=0:
-                nave.x = jx 
-            nave.x-=300*dt
+            if nave.x <= 0:
+                nave.x = jx - nave.width # Ajustado para não brotar totalmente fora
+            nave.x -= 300 * dt
         if keyboard.key_pressed("RIGHT"):
-            if nave.x>=jx:
-                nave.x=0 
-            nave.x+=300*dt
+            if nave.x >= jx:
+                nave.x = 0 
+            nave.x += 300 * dt
 
-        #atirando    
-        if keyboard.key_pressed("SPACE") and time>=intervalo_tiro:
-                print("tiro")
-                shoot_sound.play()
-                novo_tiro = Sprite("spaceinvaders/images/nave_tiro.png")
-                novo_tiro.x = nave.x+(nave.width/2) - (novo_tiro.width/2)
-                novo_tiro.y = nave.y - novo_tiro.height
-                #tiros.append(nave_tiro)
-                tiros.append(novo_tiro)
-                print(len(tiros))
-                time = 0
+        # Atirando    
+        if keyboard.key_pressed("SPACE") and time >= intervalo_tiro:
+            shoot_sound.play()
+            novo_tiro = Sprite("spaceinvaders/images/nave_tiro.png")
+            novo_tiro.x = nave.x + (nave.width/2) - (novo_tiro.width/2)
+            novo_tiro.y = nave.y - novo_tiro.height
+            tiros.append(novo_tiro)
+            time = 0
 
+        # Movimentando e desenhando tiros
         for tiro in tiros:
-            tiro.y -= vel_tiro*dt
+            tiro.y -= vel_tiro * dt
             tiro.draw()
-        for tiro in tiros:
-            if tiro.y<-tiro.height:
-                tiros.remove(tiro)
-
-        #animando inimigo
-        if intervalos_inimigas == 50:
+            
+        # Criando inimigos (Corrigido o >=)
+        if time2 >= intervalos_inimigas:
             inimiga = Sprite("enemy.png")
-            #inimiga.set_position(pos, 0)
-            inimiga.x = pos
-            inimiga.y = 0
-            #inimiga.draw()
+            # Sorteio dinâmico para não nascerem no mesmo lugar
+            inimiga.x = random.randint(0, int(jx - inimiga.width))
+            inimiga.y = -inimiga.height
             inimigas.append(inimiga)
-            intervalos_inimigas = 0
+            time2 = 0
+
+        # Movimentando e desenhando inimigos (Adicionado * dt)
         for n_inimiga in inimigas:
-            n_inimiga.y += vel_inimiga
+            n_inimiga.y += vel_inimiga * dt
             n_inimiga.draw()
-        for n_inimiga in inimigas:
-            if n_inimiga.y>jy:      
-                inimigas.remove(n_inimiga)
+            
+        # Remoção segura de objetos fora da tela (List Comprehension) --nao
+
+        tiros = [tiro for tiro in tiros if tiro.y >= -tiro.height]
+        inimigas = [n_inimiga for n_inimiga in inimigas if n_inimiga.y <= jy]
+
+        #detectando colisoes
+        for tiro in tiros:
+            for inimiga in inimigas:
+                if Collision.collided(tiro, inimiga):
+                    explosion_sound.play()
+                    acertos += 1
+                    tiros.remove(tiro)
+                    inimigas.remove(inimiga)
+        for inimiga in inimigas:
+            if Collision.collided(inimiga, nave):
+                run = False
+                os.system('clear')
+                print("NAVE ABATIDA!")
+                print("Aviões derrubados: ", acertos)
 
         nave.draw()
-        
         janela.update()
 
 jogar(800, 600, 'facil')
-
-
-
-
-
-
-
-
-    
